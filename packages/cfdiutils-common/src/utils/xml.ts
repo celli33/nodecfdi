9f00dbfe4495a4c469dc8a817accc6d8f291d579
Fernando IL <luffynando@gmail.com>
2021-11-28 22:24:14 -0600

    feat(package:common): xml utils, only valid xml name

---
+++ packages/cfdiutils-common/src/utils/xml.ts
@@ -0,0 +1,6 @@
+export class Xml {
+  public static isValidXmlName(name: string) : boolean {
+    if (name === '') return false;
+    return /^[\p{L}_][\p{L}.\d_-]*$/u.test(name);
+  }
+}
---
+++ packages/cfdiutils-common/tests/unit/utils-xml.test.ts
@@ -0,0 +1,11 @@
+import { Xml } from '../../src/utils/xml';
+
+describe('Utils.Xml', () => {
+  test.each([['First_Name'], ['_4-lane'], ['tél'], ['month-day']])('true on valid names', (name) => {
+    expect(Xml.isValidXmlName(name)).toBeTruthy();
+  });
+
+  test.each([['Driver´s_License'], ['month/day'], ['first name'], ['4-lane']])('false on invalid names', (name) => {
+    expect(Xml.isValidXmlName(name)).toBeFalsy();
+  });
+});
