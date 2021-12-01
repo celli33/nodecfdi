0c12abc585433fb0b0c99c99f8489f29ff913ab9
Fernando Isidro L <luffynando@gmail.com>
2021-12-01 09:43:10 -0600

    feat(package:elements): added Cfdi33/CfdiRelacionado

--- packages/cfdiutils-elements/src/cfdi33.ts
+++ packages/cfdiutils-elements/src/cfdi33.ts
@@ -3,3 +3,4 @@ export * from './cfdi33/receptor';
 export * from './cfdi33/traslado';
 export * from './cfdi33/retencion';
 export * from './cfdi33/addenda';
+export * from './cfdi33/cfdi_relacionado';
