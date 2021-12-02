f240075616c02ef578b67ac37f4068e3de64ba9d
Fernando Isidro L <luffynando@gmail.com>
2021-12-02 15:28:52 -0600

    fix(package:common): fixed if nodo is null in all validators

--- packages/cfdiutils-common/src/utils/dom_validators.ts
+++ packages/cfdiutils-common/src/utils/dom_validators.ts
@@ -1,17 +1,17 @@
 export class DomValidators {
   public static isElement(nodo: Node): nodo is Element {
-    return nodo.nodeType === 1;
+    return nodo && nodo.nodeType === 1;
   }
 
   public static isAttr(nodo: Node): nodo is Attr {
-    return nodo.nodeType === 2;
+    return nodo && nodo.nodeType === 2;
   }
 
   public static isText(nodo: Node): nodo is Text {
-    return nodo.nodeType === 3;
+    return nodo && nodo.nodeType === 3;
   }
 
   public static isDocument(nodo: Node): nodo is Document {
-    return nodo.nodeType === 9;
+    return nodo && nodo.nodeType === 9;
   }
 }
