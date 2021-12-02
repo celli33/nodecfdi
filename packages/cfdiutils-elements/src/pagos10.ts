bf43b639baf53cac5f784a4a6b9f0e1550b88d25
Fernando Isidro L <luffynando@gmail.com>
2021-12-02 01:01:25 -0600

    feat(package:elements): added Pagos10/Traslados

--- packages/cfdiutils-elements/src/pagos10.ts
+++ packages/cfdiutils-elements/src/pagos10.ts
@@ -2,3 +2,4 @@ export * from './pagos10/docto_relacionado';
 export * from './pagos10/retencion';
 export * from './pagos10/retenciones';
 export * from './pagos10/traslado';
+export * from './pagos10/traslados';
