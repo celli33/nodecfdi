94a7df01a9ea5bf83d39cc2940f2c666ba328b61
Fernando Isidro L <luffynando@gmail.com>
2021-12-02 02:11:42 -0600

    feat(package:elements): added Pagos10/Impuestos

--- packages/cfdiutils-elements/src/pagos10.ts
+++ packages/cfdiutils-elements/src/pagos10.ts
@@ -3,3 +3,4 @@ export * from './pagos10/retencion';
 export * from './pagos10/retenciones';
 export * from './pagos10/traslado';
 export * from './pagos10/traslados';
+export * from './pagos10/impuestos';
