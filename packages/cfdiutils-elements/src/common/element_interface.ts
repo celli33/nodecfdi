c6f7be833c0b314a2a338c29bd4101bc6a382602
Fernando Isidro L <luffynando@gmail.com>
2021-12-08 02:56:09 -0600

    refactor: remove static code packages/cfdiutils-elements

--- packages/cfdiutils-elements/src/common/element_interface.ts
+++
@@ -1,9 +0,0 @@
-import { CNodeInterface } from '@nodecfdi/cfdiutils-common';
-
-export interface ElementInterface extends CNodeInterface {
-  getElementName(): string;
-
-  getFixedAttributes(): Record<string, string>;
-
-  getChildrenOrder(): string[];
-}
